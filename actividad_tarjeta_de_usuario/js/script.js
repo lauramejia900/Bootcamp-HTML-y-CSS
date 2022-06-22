8af72c43793c92afd575112c3a4fcd40c06b6bb9
Laura Mejia <lauramejia900@gmail.com>
2022-06-22 14:32:21 -0500

    first commit

---
+++ actividad_tarjeta_de_usuario/js/script.js
@@ -0,0 +1,7 @@
+/*Evento onclick: detecta cuando alguien hace click en algún elemento */
+function hola_mundo(){
+  /*  console.log("Hola");
+    console.log("como estas?");*/
+    /*Alerta */
+    alert("Hola");
+}
